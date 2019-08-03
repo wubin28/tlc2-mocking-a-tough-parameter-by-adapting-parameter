978a2524b61a7a51b6766ddfeb8914ef7e364acc
benwu <wubinben@icloud.com>
2019-08-03 16:45:38 +0800

    mock HttpServletRequest directly

--- src/test/java/kata/taminglegacycode/tlc2/ARMDispatcherTest.java
+++ src/test/java/kata/taminglegacycode/tlc2/ARMDispatcherTest.java
@@ -1,9 +1,28 @@
 package kata.taminglegacycode.tlc2;
 
+import org.hamcrest.collection.IsMapContaining;
 import org.junit.Test;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 public class ARMDispatcherTest {
     @Test
     public void value_of_page_state_name_should_be_put_in_market_bindings() {
-    }
+        Map<String, String> marketBindings = new HashMap<>();
+        String pageStateName = "normal";
+        HttpServletRequest source = mock(HttpServletRequest.class);
+        given(source.getParameterValues("normal")).willReturn(new String[]{"9"});
+        ARMDispatcher armDispatcher = new ARMDispatcher();
+
+        armDispatcher.populate(source, pageStateName, marketBindings);
+
+        assertThat(marketBindings.size(), is(1));
+        assertThat(marketBindings, IsMapContaining.hasValue("9"));    }
 }
